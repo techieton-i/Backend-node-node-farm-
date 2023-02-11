23bf8f0bd18ae0ebf531d9c1d0d62a873a6a010d
israel <odejobiisrael@gmail.com>
2023-02-11 22:11:51 +0100

    removed comments

--- index.js
+++ index.js
@@ -40,47 +40,6 @@ const replaceTemplate = require("./modules/replaceTemplate");
 //////////////////////////////
 //SERVER
 
-// const server = createServer((req, res) => {
-//   const { query, pathname } = url.parse(req.url, true);
-
-//   // Overview page
-//   if (pathname === "/" || pathname === "/overview") {
-//     res.writeHead(200, {
-//       "Content-type": "text/html",
-//     });
-
-//     const cardsHtml = dataObj
-//       .map((el) => replaceTemplate(tempCard, el))
-//       .join("");
-//     const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
-//     res.end(output);
-
-//     // Product page
-//   } else if (pathname === "/product") {
-//     res.writeHead(200, {
-//       "Content-type": "text/html",
-//     });
-//     const product = dataObj[query.id];
-//     const output = replaceTemplate(tempProduct, product);
-//     res.end(output);
-
-//     // API
-//   } else if (pathname === "/api") {
-//     res.writeHead(200, {
-//       "Content-type": "application/json",
-//     });
-//     res.end(data);
-
-//     // Not found
-//   } else {
-//     res.writeHead(404, {
-//       "Content-type": "text/html",
-//       "my-own-header": "hello-world",
-//     });
-//     res.end("<h1>Page not found!</h1>");
-//   }
-// });
-
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
   "utf-8"
